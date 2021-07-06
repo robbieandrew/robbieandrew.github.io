3f78e28ddbf4f1a98fcc75a5f814ff8b4999c713
Robbie Andrew <robbie.andrew@cicero.oslo.no>
2021-07-06 20:09:29 +0200

    minor

--- india/js/includeHTML.js
+++ india/js/includeHTML.js
@@ -1,7 +1,8 @@
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
+  /*z = document.getElementsByTagName("*");*/
+  z = document.querySelectorAll("div.replaceme");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
