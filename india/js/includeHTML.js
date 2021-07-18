a760a623970529feb3c267f96169b6e848a82237
Robbie Andrew <robbie.andrew@cicero.oslo.no>
2021-07-18 09:51:19 +0200

    Update includeHTML.js

--- india/js/includeHTML.js
+++ india/js/includeHTML.js
@@ -2,8 +2,8 @@
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  /*z = document.querySelectorAll("div.includehtml");*/
+  /*z = document.getElementsByTagName("*");*/
+  z = document.querySelectorAll("div.includehtml");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
