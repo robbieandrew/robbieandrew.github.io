1f8c8ffd9d3cd6084f7204e7b8b831de93d9ed66
Robbie Andrew <robbie.andrew@cicero.oslo.no>
2021-07-18 09:35:27 +0200

    First test of update text

--- india/js/includeHTML.js
+++ india/js/includeHTML.js
@@ -17,6 +17,7 @@ function includeHTML() {
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute("class");
           includeHTML();
         }
       }      
