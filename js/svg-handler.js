f182176ea06ebf6f3fae31ae3132134fc2714544
robbieandrew <65347420+robbieandrew@users.noreply.github.com>
2026-03-31 13:48:20 +0200

    Spinner.

--- js/svg-handler.js
+++ js/svg-handler.js
@@ -1,4 +1,5 @@
-  let availableDataFiles = [];
+let availableDataFiles = [];
+
 
   document.addEventListener("DOMContentLoaded", async () => {
     const url_path = window.location.pathname;
@@ -6,7 +7,16 @@
 	// obtaining the list of data files could sometimes take a while, allowing some SVGs to already be loaded before it's finished. In that case we can't add an event listener to process the SVG after it's loaded, because it's already loaded. So below we handle these two cases: SVG already loaded, then process it; SVG not yet loaded, add an event listener.
 	const loaded = await loadSiteDataFiles("data",parts[0]);
 
-	document.querySelectorAll('object[type="image/svg+xml"]').forEach(svgObject => {
+	const svgObjects = document.querySelectorAll('object[type="image/svg+xml"]');
+    let svgsToLoad = svgObjects.length;
+
+    // If there are no SVGs on the page, kill the loader immediately
+    if (svgsToLoad === 0) {
+        document.getElementById('page-loader')?.remove();
+        return;
+    }
+
+	svgObjects.forEach(svgObject => {
 	  function handleSVGLoad() {
 		addSVGbuttons(svgObject);
 		addSVGmetadata(svgObject);
@@ -24,6 +34,12 @@
 		if (container) {
 		  container.classList.add("figure-group");
 		}
+		
+		// Decrement and check
+		svgsToLoad--;
+		if (svgsToLoad <= 0) {
+			document.getElementById('page-loader')?.remove();
+		}
 	  }
 
 	  if (svgObject.contentDocument && svgObject.contentDocument.rootElement) {
@@ -34,5 +50,20 @@
 	  }
 	});
 	
+	setTimeout(() => {
+        const loader = document.getElementById('page-loader');
+        if (loader) {
+            const loaderText = loader.querySelector('.loader-text');
+            if (loaderText) {
+                loaderText.innerText = "Almost there..."; 
+            }
+            
+            // Wait 2 more seconds so they can read the message, then fade out
+            setTimeout(() => {
+                loader.style.opacity = '0';
+                setTimeout(() => loader.remove(), 500);
+            }, 2000);
+        }
+    }, 10000); // Trigger after 10 seconds of hanging	
   });
 
