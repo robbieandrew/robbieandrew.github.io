7832673024fba83dcc708bf063649bf6266ec063
robbieandrew <65347420+robbieandrew@users.noreply.github.com>
2026-05-27 07:26:16 +0200

    .

--- js/downloadSVGasPNG.js
+++ js/downloadSVGasPNG.js
@@ -529,7 +529,7 @@ function createCopyLink(element) {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
          fill="none" stroke="currentColor" stroke-width="2"
          stroke-linecap="round" stroke-linejoin="round"
-         style="width:1em; height:1em; vertical-align:-0.15em; margin-right:0.3em; line-height:0;">
+         style="width:1em; height:1em; margin-right:0.3em; line-height:0;">
       <rect x="9" y="9" width="13" height="13" rx="2"/>
       <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
     </svg>Copy`;
